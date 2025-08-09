1043df86d342b0b95d3f007aabb22dc8329c7257
Ajay Gaud <gaudajay399@gmail.com>
2025-08-09 11:28:13 +0530

    Added password hasher function

---
+++ DatabaseRepository/Common/Utilities/PasswordHasher.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+
+namespace DatabaseRepository.Common.Utilities
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            using var rng = RandomNumberGenerator.Create();
+            byte[] salt = new byte[16];
+            rng.GetBytes(salt);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100_000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(32);
+
+            // Combine salt + hash for storage
+            byte[] hashBytes = new byte[48];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 32);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            byte[] hashBytes = Convert.FromBase64String(hashedPassword);
+
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100_000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(32);
+
+            for (int i = 0; i < 32; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
