2f834e0dbc8ca034ae5940aa5ecc1280fd8790f2
Ajay Gaud <gaudajay399@gmail.com>
2025-08-11 18:39:45 +0530

    Added new constants

--- DatabaseRepository/Constants/BaseConstant.cs
+++ DatabaseRepository/Constants/BaseConstant.cs
@@ -6,5 +6,8 @@
         public const string AuthenticationConfig = "AuthenticationConfig";
         public const string IsDeleted = "IsDeleted";
         public const string _id = "_id";
+        public const string Admin = "Admin";
+        public const string Name = "Name";
+        public const string Standard = "Standard";
     }
 }
