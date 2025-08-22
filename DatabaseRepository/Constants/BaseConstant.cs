0a08815def796e705bd6fe961d8503c3be14dcd9
codename399 <gaudajay399@gmail.com>
2025-08-22 21:55:39 +0530

    Added basic constants

--- DatabaseRepository/Constants/BaseConstant.cs
+++ DatabaseRepository/Constants/BaseConstant.cs
@@ -10,5 +10,14 @@
         public const string Name = "Name";
         public const string Standard = "Standard";
         public const string CreationDateTime = "CreationDateTime";
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Username = "Username";
+        public const string UserId = "UserId";
+        public const string EmailId = "EmailId";
+        public const string ContactNumber = "ContactNumber";
+        public const string Description = "Description";
+        public const string Route = "Route";
+        public const string PasswordBasedAuthenticationConfig = "PasswordBasedAuthenticationConfig";
+        public const string OTPBasedAuthenticationConfig = "OTPBasedAuthenticationConfig";
     }
 }
