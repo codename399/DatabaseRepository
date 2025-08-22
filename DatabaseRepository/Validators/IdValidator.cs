a7b1a1feb10a4e9db0d3bcbc4c883f9bf14d0e13
codename399 <gaudajay399@gmail.com>
2025-08-22 22:26:50 +0530

    Added fluent validator

---
+++ DatabaseRepository/Validators/IdValidator.cs
@@ -0,0 +1,15 @@
+﻿using DatabaseRepository.Common.Utilities;
+using FluentValidation;
+
+namespace DatabaseRepository.Validators
+{
+    public class IdValidator : AbstractValidator<string>
+    {
+        public IdValidator()
+        {
+            RuleFor(x => x)
+                    .NotNull()
+            .Must(Util.IsValidGuid);
+        }
+    }
+}
