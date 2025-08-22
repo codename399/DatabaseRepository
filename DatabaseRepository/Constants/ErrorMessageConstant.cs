1026a1b05d766c5234b575b806a115d880ac7549
codename399 <gaudajay399@gmail.com>
2025-08-22 21:53:39 +0530

    Added error message constants file

---
+++ DatabaseRepository/Constants/ErrorMessageConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace DatabaseRepository.Constants
+{
+    public static class ErrorMessageConstant
+    {
+        public const string EmptyList = "The list is empty.";
+        public const string NullParameter = "The provided prarmeter is null.";
+    }
+}
