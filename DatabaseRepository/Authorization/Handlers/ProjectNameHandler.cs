bf7604c03acee84885b516b62b04504181fb8198
codename399 <gaudajay399@gmail.com>
2025-08-23 11:32:59 +0530

    Added admin check in policy handler

--- DatabaseRepository/Authorization/Handlers/ProjectNameHandler.cs
+++ DatabaseRepository/Authorization/Handlers/ProjectNameHandler.cs
@@ -1,5 +1,7 @@
 ﻿using DatabaseRepository.Authorization.Requirements;
+using DatabaseRepository.Constants;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace DatabaseRepository.Authorization.Handlers
 {
@@ -7,7 +9,14 @@ namespace DatabaseRepository.Authorization.Handlers
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ProjectNameRequirement requirement)
         {
-            if (context.User.HasClaim(c => c.Type == requirement.ProjectName))
+            if (!context.User.HasClaim(c => c.Type == ClaimTypes.Role))
+            {
+                return Task.CompletedTask;
+            }
+
+            string role = context.User.FindFirst(c => c.Type == ClaimTypes.Role)?.Value ?? string.Empty;
+
+            if (context.User.HasClaim(c => c.Type == requirement.ProjectName) || role == BaseConstant.Admin)
             {
                 context.Succeed(requirement);
             }
